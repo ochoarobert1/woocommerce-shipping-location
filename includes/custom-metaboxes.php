cfdf2901af00c4944e6bbb7c34238b323c362288
Robert Ochoa <ochoa.robert1@gmail.com>
2021-07-07 17:19:17 -0400

    Fixes in Custom Metaboxes

--- includes/custom-metaboxes.php
+++ includes/custom-metaboxes.php
@@ -15,6 +15,7 @@ class WooShipLocationMetabox extends WooShipLocation
     public function __construct()
     {
         add_action('add_meta_boxes', array($this, 'custom_metaboxes'), 0);
+        add_action('save_post', array($this, 'save_postdata'));
     }
 
     public function custom_metaboxes()
@@ -22,10 +23,10 @@ class WooShipLocationMetabox extends WooShipLocation
         $screens = [ 'location' ];
         foreach ($screens as $screen) {
             add_meta_box(
-                'location_box_id',                 // Unique ID
-                __('Extra Information', parent::PLUGIN_LANG),      // Box title
-                array($this, 'custom_box_html'),  // Content callback, must be of type callable
-                $screen                            // Post type
+                'location_box_id',
+                __('Extra Information', parent::PLUGIN_LANG),
+                array($this, 'custom_box_html'),
+                $screen
             );
         }
     }
@@ -35,23 +36,62 @@ class WooShipLocationMetabox extends WooShipLocation
         ?>
 <div class="custom-metaboxes-table">
     <div class="custom-metaboxes-table-item">
+        <?php $min_distance = get_post_meta($post->ID, 'min_distance', true); ?>
         <label for="min_distance" class="custom-label-attributes"><?php _e('Minimun Distance (in km)', parent::PLUGIN_LANG) ?></label>
-        <input class="custom-input-attributes" type="number" name="min_distance" id="min_distance" />
+        <input class="custom-input-attributes" type="number" name="min_distance" id="min_distance" value="<?php echo $min_distance; ?>" />
     </div>
     <div class="custom-metaboxes-table-item">
+        <?php $max_distance = get_post_meta($post->ID, 'max_distance', true); ?>
         <label for="max_distance" class="custom-label-attributes"><?php _e('Maximum Distance (in km)', parent::PLUGIN_LANG) ?></label>
-        <input class="custom-input-attributes" type="number" name="max_distance" id="max_distance" />
+        <input class="custom-input-attributes" type="number" name="max_distance" id="max_distance" value="<?php echo $max_distance; ?>" />
     </div>
     <div class="custom-metaboxes-table-item">
+        <?php $email_address = get_post_meta($post->ID, 'email_address', true); ?>
         <label for="email_address" class="custom-label-attributes"><?php _e('Email Address', parent::PLUGIN_LANG) ?></label>
-        <input class="custom-input-attributes" type="email" name="email_address" id="email_address" />
+        <input class="custom-input-attributes" type="email" name="email_address" id="email_address" value="<?php echo $email_address; ?>" />
     </div>
     <div class="custom-metaboxes-table-item">
+        <?php $coordinates = get_post_meta($post->ID, 'min_distance', true); ?>
         <label for="coordinates" class="custom-label-attributes"><?php _e('Coordinates (in Google Maps)', parent::PLUGIN_LANG) ?></label>
-        <input class="custom-input-attributes" type="text" name="coordinates" id="coordinates" placeholder="<?php _e('E.G. 25.7825452,-80.2996705', parent::PLUGIN_LANG); ?>" />
+        <input class="custom-input-attributes" type="text" name="coordinates" id="coordinates" placeholder="<?php _e('E.G. 25.7825452,-80.2996705', parent::PLUGIN_LANG); ?>" value="<?php echo $coordinates; ?>" />
     </div>
 </div>
 <?php
+    }
+
+    public function save_postdata($post_id)
+    {
+        if (array_key_exists('min_distance', $_POST)) {
+            update_post_meta(
+                $post_id,
+                'min_distance',
+                $_POST['min_distance']
+            );
+        }
+
+        if (array_key_exists('max_distance', $_POST)) {
+            update_post_meta(
+                $post_id,
+                'max_distance',
+                $_POST['max_distance']
+            );
+        }
+
+        if (array_key_exists('email_address', $_POST)) {
+            update_post_meta(
+                $post_id,
+                'email_address',
+                $_POST['email_address']
+            );
+        }
+
+        if (array_key_exists('coordinates', $_POST)) {
+            update_post_meta(
+                $post_id,
+                'coordinates',
+                $_POST['coordinates']
+            );
+        }
     }
 }
 
